021afa2279badf8b9152c8acc1d5eaab40a0306b
Verbena Macbook Air <vassidiqi@gmail.com>
2025-11-20 06:21:20 +0700

    Implement verify password

--- bff/src/handlers/auth_handler.rs
+++ bff/src/handlers/auth_handler.rs
@@ -1,53 +1,71 @@
 use std::sync::Arc;
 
-use axum::{Json, extract::State, debug_handler};
-use chrono::Utc;
-use crate::{AppState, dtos::{auth_dto, general_res_dto::GeneralResDto}, models::user::NewUser, services::auth_service::hash_password};
 use crate::error::CustomError;
+use crate::{
+    AppState,
+    dtos::{auth_dto, general_res_dto::GeneralResDto},
+    models::user::NewUser,
+    services::auth_service::{hash_password, verify_password},
+};
+use axum::{Json, debug_handler, extract::State};
+use chrono::Utc;
 
 #[debug_handler]
-pub async fn register(State(state): State<Arc<AppState>>, Json(payload): Json<auth_dto::RegisterReqDto>) -> Result<Json<GeneralResDto>, CustomError> {
-    
-    if payload.email.is_empty() || !payload.email.contains("@") || payload.username.len() < 5 || payload.password.len() < 8 {
+pub async fn register(
+    State(state): State<Arc<AppState>>,
+    Json(payload): Json<auth_dto::RegisterReqDto>,
+) -> Result<Json<GeneralResDto>, CustomError> {
+    if payload.email.is_empty()
+        || !payload.email.contains("@")
+        || payload.username.len() < 5
+        || payload.password.len() < 8
+    {
         return Err(CustomError::MissingCredentials);
     }
 
     let password_hash = match hash_password(payload.password) {
         Ok(value) => value,
-        Err(_) => return Err(CustomError::HashError)
+        Err(_) => return Err(CustomError::HashError),
     };
 
     // let password_hash = hash_password(payload.password)
     // .map_err(|_| CustomError::HashError)?;
 
     let user = NewUser {
-        email: payload.email,
+        email: payload.email.to_lowercase(),
         username: payload.username,
         password: password_hash,
         createdAt: Utc::now(),
-        updatedAt: Utc::now()
+        updatedAt: Utc::now(),
     };
-    
+
     state.user_service.create_user(&user).await?;
 
     Ok(Json(GeneralResDto {
         message: "Ok".to_string(),
-        status_code: 201
+        status_code: 201,
     }))
 }
 
-pub async fn login(Json(payload): Json<auth_dto::LoginReqDto>) -> Result<Json<GeneralResDto>, CustomError> {
+pub async fn login(
+    State(state): State<Arc<AppState>>,
+    Json(payload): Json<auth_dto::LoginReqDto>,
+) -> Result<Json<GeneralResDto>, CustomError> {
     if payload.email.is_empty() || !payload.email.contains("@") || payload.password.len() < 8 {
         return Err(CustomError::MissingCredentials);
     }
 
-    Ok(Json(GeneralResDto {
-        message: "Ok".to_string(),
-        status_code: 201
-    }))
+    let user = state.user_service.get_user_by_email(&payload.email).await?;
+
+    match verify_password(payload.password, user.password) {
+        Ok(_) => Ok(Json(GeneralResDto {
+            message: "Ok".to_string(),
+            status_code: 201,
+        })),
+        Err(_) => Err(CustomError::WrongCredentials)
+    }
 }
 
 pub async fn logout() {
     // Implement logout
 }
-
--- bff/src/services/user_service.rs
+++ bff/src/services/user_service.rs
@@ -1,6 +1,13 @@
-use mongodb::{Database, error::{ErrorKind, WriteFailure}};
+use bson::doc;
+use mongodb::{
+    Database,
+    error::{ErrorKind, WriteFailure},
+};
 
-use crate::{error::CustomError, models::user::NewUser};
+use crate::{
+    error::CustomError,
+    models::user::{NewUser, User},
+};
 
 pub struct UserService {
     pub db: Database,
@@ -21,19 +28,33 @@ impl UserService {
             Ok(value) => {
                 println!("Created a user with _id: {}", value.inserted_id);
                 Ok(())
-            },
+            }
             Err(error) => {
                 eprintln!("Error inserting document: {}", error);
 
                 match error.kind.as_ref() {
                     ErrorKind::Write(WriteFailure::WriteError(w)) if w.code == 11000 => {
                         Err(CustomError::DuplicateKey(data.email.clone()))
-                    },
-                    _ => Err(CustomError::MongoError(error))
+                    }
+                    _ => Err(CustomError::MongoError(error)),
                 }
             }
         }
     }
 
-    // pub async fn get_user_by_email(&self, email: &'static string) ->
+    pub async fn get_user_by_email(&self, email: &str) -> Result<User, CustomError> {
+        match self
+            .db
+            .collection::<User>("users")
+            .find_one(doc! { "email": email.to_lowercase() })
+            .await
+        {
+            Ok(Some(user)) => Ok(user),
+            Ok(None) => Err(CustomError::NotFoundError(email.to_string())),
+            Err(err) => {
+                eprintln!("Error finding user: {}", err);
+                Err(CustomError::MongoError(err))
+            }
+        }
+    }
 }
