8d3b572034e6e62d852f6dcb37c6fbd521ef103a
Verbena Macbook Air <vassidiqi@gmail.com>
2025-12-08 23:17:05 +0700

    Attempt to replace symmetric with asymmetric jwt

--- bff/src/types/keys.rs
+++ bff/src/types/keys.rs
@@ -1,3 +1,4 @@
+use base64::{Engine, engine::general_purpose};
 use jsonwebtoken::{DecodingKey, EncodingKey};
 use std::{env::var, sync::LazyLock};
 
@@ -7,15 +8,21 @@ pub struct Keys {
 }
 
 impl Keys {
-    pub fn new(secret: &[u8]) -> Self {
+    pub fn new(private: Vec<u8>, public: Vec<u8>) -> Self {
         Self {
-            encoding: EncodingKey::from_secret(secret),
-            decoding: DecodingKey::from_secret(secret),
+            encoding: EncodingKey::from_ed_pem(&private).expect("Invalid private Ed25519 PEM"),
+            decoding: DecodingKey::from_ed_pem(&public).expect("Invalid public Ed25519 PEM"),
         }
     }
 }
 
 pub static KEYS: LazyLock<Keys> = LazyLock::new(|| {
-    let secret = var("JWT_SECRET").expect("JWT_SECRET must be set");
-    Keys::new(secret.as_bytes())
+    // let secret = var("JWT_SECRET").expect("JWT_SECRET must be set");
+    let private_b64 = var("JWT_PRIVATE_KEY").expect("JWT_PRIVATE_KEY missing");
+    let public_b64 = var("JWT_PUBLIC_KEY").expect("JWT_PUBLIC_KEY missing");
+
+    let private_pem = general_purpose::STANDARD.decode(&private_b64).expect("Ivalid private key base64");
+    let public_pem = general_purpose::STANDARD.decode(&public_b64).expect("Ivalid public key base64");
+
+    Keys::new(private_pem, public_pem)
 });
